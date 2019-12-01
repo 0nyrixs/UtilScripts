5992f84942434f61238f6833c6bd5738315efc2a
0nyrix <sp4iwn@protonmail.com>
2019-12-01 11:48:02 +0100

    Update androidsource.sh

--- androidsource.sh
+++ androidsource.sh
@@ -9,8 +9,8 @@ chmod a+x ~/bin/repo
 mkdir ~/WORKING_DIRECTORY
 cd ~/WORKING_DIRECTORY
 
-git config --global user.name "Another Man"
-git config --global user.email "mairenajuanete@gmail.com"
+git config --global user.name $1
+git config --global user.email $2
 
 repo init --depth=1 -u https://android.googlesource.com/platform/manifest
 repo sync  -f --no-clone-bundle --no-tags -j3
